fe4bdf3a8313639405faeaf83165a37d2d249323
Brendan Travis <brendantravis1@gmail.com>
2021-11-24 12:21:26 +0000

    Finish content for pass component set

--- src/pass/components/about-me.vue
+++ src/pass/components/about-me.vue
@@ -2,57 +2,21 @@
   <div class="card">
     <h2>About Me</h2>
     <p>
-      A wonderful serenity has taken possession of my entire soul, like these
-      sweet mornings of spring which I enjoy with my whole heart. I am alone,
-      and feel the charm of existence in this spot, which was created for the
-      bliss of souls like mine. I am so happy, my dear friend, so absorbed in
-      the exquisite sense of mere tranquil existence, that I neglect my talents.
-      I should be incapable of drawing a single stroke at the present moment;
-      and yet I feel that I never was a greater artist than now.
+      Brendan is a full stack developer that cut his teeth on C# and TypeScript
+      development, working with cloud technologies, .NET APIs, and frontend
+      JavaScript Frameworks. An independent worker capable of working across
+      multi-disciplined teams and mentoring junior members with a focus on Agile
+      iterative development cycles.
     </p>
     <p>
-      When, while the lovely valley teems with vapour around me, and the
-      meridian sun strikes the upper surface of the impenetrable foliage of my
-      trees, and but a few stray gleams steal into the inner sanctuary, I throw
-      myself down among the tall grass by the trickling stream; and, as I lie
-      close to the earth, a thousand unknown plants are noticed by me: when I
-      hear the buzz of the little world among the stalks, and grow familiar with
-      the countless indescribable forms of the insects and flies, then I feel
-      the presence of the Almighty, who formed us in his own image, and the
-      breath of that universal love which bears and sustains us, as it floats
-      around us in an eternity of bliss; and then, my friend, when darkness
-      overspreads my eyes, and heaven and earth seem to dwell in my soul and
-      absorb its power, like the form of a beloved mistress, then I often think
-      with longing, Oh, would I could describe these conceptions, could impress
-      upon paper all that is living so full and warm within me, that it might be
-      the mirror of my soul, as my soul is the mirror of the infinite God! O my
-      friend -- but it is too much for my strength -- I sink under the weight of
-      the splendour of these visions! A wonderful serenity has taken possession
-      of my entire soul, like these sweet mornings of spring which I enjoy with
-      my whole heart. I am alone, and feel the charm of existence in this spot,
-      which was created for the bliss of souls like mine.
+      Whether it be tweaking kubernetes containers on a cloud estate or
+      perfectly aligning components on the front-end, Brendan has a high
+      attention to detail and always looking for new technology opportunities to
+      further his learning.
     </p>
-    <p>
-      I am so happy, my dear friend, so absorbed in the exquisite sense of mere
-      tranquil existence, that I neglect my talents. I should be incapable of
-      drawing a single stroke at the present moment; and yet I feel that I never
-      was a greater artist than now. When, while the lovely valley teems with
-      vapour around me, and the meridian sun strikes the upper surface of the
-      impenetrable foliage of my trees, and but a few stray gleams steal into
-      the inner sanctuary, I throw myself down among the tall grass by the
-      trickling stream; and, as I lie close to the earth, a thousand unknown
-      plants are noticed by me: when I hear the buzz of the little world among
-      the stalks, and grow familiar with the countless indescribable forms of
-      the insects and flies, then I feel the presence of the Almighty, who
-      formed us in his own image, and the breath of that universal love which
-      bears and sustains us, as it floats around us in an eternity of bliss; and
-      then, my friend, when darkness overspreads my eyes, and heaven and earth
-      seem to dwell in my soul and absorb its power, like the form of a beloved
-      mistress, then I often think with longing, Oh, would I could describe
-      these conceptions, could impress upon paper all that is living so full and
-      warm within me, that it might be the mirror of my soul, as my soul is the
-      mirror of the infinite God! O my friend -- but it is too much for my
-      strength -- I sink under the weight of the splendour of these visions!
+    <p class="tags">
+      Azure, AWS, Kubernetes, Docker, C#, .NET (Framework, Core), TypeScript,
+      JavaScript, HTML, CSS/SCSS, Angular, Vue
     </p>
   </div>
 </template>
@@ -77,8 +41,16 @@ export default defineComponent({});
     margin-right: 5%;
   }
 
+  p {
+    text-align: justify;
+  }
+
   h2 {
     color: $col-base-1;
+  }
+
+  .tags {
+    font-size: 0.75rem;
   }
 }
 
--- src/pass/components/project-list.vue
+++ src/pass/components/project-list.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="container">
     <h2>Personal Projects</h2>
-    <a class="card" href="https://github.com/brendan-travis/api-playground">
+    <div class="card">
       <img class="image" src="@/assets/api_playground.png" />
       <div class="content">
         <h3>API Playground</h3>
-        <p>A .NET API built for testing purposes</p>
+        <p>
+          A .NET API built for testing purposes with the aim to provide a fully
+          functional API to mock backend functionality when developing or
+          testing frontend systems.
+        </p>
+        <p>
+          Built with a fully orchestrate container environment with Kubernetes
+          and numerous build scripts to provide a local interpretation of a CD
+          pipeline.
+        </p>
+        <a href="https://github.com/brendan-travis/api-playground">
+          Click to view the source code.
+        </a>
         <p class="tags">.NET, Web API, Kubernetes, Docker</p>
       </div>
-    </a>
-    <a
-      class="card"
-      href="https://brendan-travis.github.io/dark-souls-iii.quest-tracker/"
-    >
+    </div>
+    <div class="card">
       <img class="image" src="@/assets/ds3_quest_checklist.jpg" />
       <div class="content">
         <h3>Dark Souls III Quest Checklist</h3>
@@ -20,13 +29,21 @@
           A lightweight checklist to assist in completing all Dark Souls III
           quest-lines in a single play-through.
         </p>
+        <p>
+          Built with Angular as a way to learn the framework and get to grips
+          with the component level design that modern JavaScript frameworks
+          offer. The app has PWA support and manages state to provide a
+          consistent experience across reloads.
+        </p>
+        <a
+          href="https://brendan-travis.github.io/dark-souls-iii.quest-tracker/"
+        >
+          Click to view the app.
+        </a>
         <p class="tags">TypeScript, Angular, PWA, HTML, SCSS</p>
       </div>
-    </a>
-    <a
-      class="card"
-      href="https://github.com/brendan-travis/rpgmv.real-time-clock"
-    >
+    </div>
+    <div class="card">
       <img class="image" src="@/assets/rpg_maker_mv_realtime_clock.jpg" />
       <div class="content">
         <h3>RPG Maker MV Plugin: Real-Time Clock</h3>
@@ -39,13 +56,17 @@
         <p>
           The point at which the hours and days tick over can be changed by
           adjusting the plugins parameters. For example, setting the
-          HourTickover parameter to 20 will trigger the hour event when the
-          clock reaches 20 past the hour. Also setting the DayTickover parameter
-          to 6 will trigger the day event when the clock reaches 6:20.
+          <span class="code">HourTickover</span> parameter to 20 will trigger
+          the hour event when the clock reaches 20 past the hour. Also setting
+          the <span class="code">DayTickover</span> parameter to 6 will trigger
+          the day event when the clock reaches 6:20.
         </p>
+        <a href="https://github.com/brendan-travis/rpgmv.real-time-clock">
+          Click to view the source code.
+        </a>
         <p class="tags">JavaScript, RPG Maker MV</p>
       </div>
-    </a>
+    </div>
   </div>
 </template>
 
@@ -63,15 +84,21 @@ export default defineComponent({});
   margin-top: 100px;
   background: $col-base-3;
 
+  @media (max-width: $break-mobile-max) {
+    padding: 32px 10% 64px;
+  }
+
   .card {
     margin-top: 24px;
     display: flex;
     flex-direction: row;
-    text-decoration: none;
-    color: unset;
 
     @media (max-width: $break-mobile-max) {
       flex-direction: column;
+    }
+
+    a {
+      color: unset;
     }
 
     .image {
@@ -87,10 +114,23 @@ export default defineComponent({});
 
     .content {
       flex-grow: 1;
-      margin-left: 24px;
+      text-align: justify;
+
+      @media (max-width: $break-mobile-max) {
+        margin-top: 8px;
+      }
+
+      @media (min-width: $break-mobile-min) {
+        margin-left: 24px;
+      }
 
       .tags {
         font-size: 0.75rem;
+      }
+
+      .code {
+        color: $col-base-2;
+        font-weight: bold;
       }
 
       h3 {
